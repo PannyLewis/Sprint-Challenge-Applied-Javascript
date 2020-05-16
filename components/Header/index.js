8873cfaa224cbf62ba5ad1e37b29dd5cdcde2073
Panny Lewis <pannylewis@gmail.com>
2020-05-16 12:10:02 -0500

    step 1:  completed the header.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,32 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+const header = document.querySelector(".header-container");
+// console.log(header);
+header.appendChild(createHeader());
+
+function createHeader() {
+  //setting elements
+  const header = document.createElement("div");
+  const date = document.createElement("span");
+  const title = document.createElement("h1");
+  const temperature = document.createElement("span");
+
+  //setting classlist
+  header.classList.add("header");
+  date.classList.add("date");
+  title.classList.add("h1");
+  temperature.classList.add("temp");
+
+  //setting structure or append to parent
+  header.appendChild(date);
+  header.appendChild(title);
+  header.appendChild(temperature);
+
+  //add text
+  date.textContent = "MARCH 28, 2019";
+  title.textContent = "Lambda Times";
+  temperature.textContent = "98°";
+
+  return header;
+}
